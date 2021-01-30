3b8b9122059388aa3af7ddbec5a2b7555c0599e9
carametal <mkatsdevelop@gmail.com>
2021-01-30 15:38:08 +0900

    サインイン機能の簡易実装

--- insta/urls.py
+++ insta/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('signin/', views.signin, name='signin')
+    path('signin/', views.signin, name='signin'),
+    path('authentication', views.authentication, name='authentication')
 ]
--- insta/views.py
+++ insta/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
 from django.template import loader
+from django.contrib.auth import authenticate
 from insta.forms import Trial
 
 def index(request):
@@ -11,3 +12,12 @@ def index(request):
 
 def signin(request):
     return render(request, 'insta/signin.html')
+
+def authentication(request):
+    input_username = request.POST['username']
+    input_password = request.POST['password']
+    user = authenticate(username=input_username, password=input_password)
+    if user:
+        return HttpResponse("signed in.")
+    else:
+        return HttpResponse("invalid input.")
